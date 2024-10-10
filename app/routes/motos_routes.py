e7b41c6cabbc39ce57638f4bff91efa953837985
SADM <sadmous0825@gmail.com>
2024-10-10 11:43:22 -0500

    r

--- app/routes/motos_routes.py
+++ app/routes/motos_routes.py
@@ -9,3 +9,11 @@ def yz_250_view():
 @bp.route('/motos/rx_115')
 def rx_115_view():
     return render_template('motos/rx_115.html')
+
+@bp.route('/motos/dt_175')
+def dt_175_view():
+    return render_template('motos/dt_175.html')
+
+bp.route('/motos/fz_250')
+def fz_250_view():
+    return render_template('motos/fz_250.html')
